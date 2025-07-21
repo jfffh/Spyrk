e75dde08e458ba4be547bf8b9528f6c25e962234
jfffh <157326539+jfffh@users.noreply.github.com>
2025-07-21 16:58:28 +0930

    Draw module refactor and new functionality
    Added functions for outline drawing. Refactored old functions to allow for application onto individual surfaces, as well as regular sprites.

--- engine/core/draw.py
+++ engine/core/draw.py
@@ -1,26 +1,30 @@
 import pygame
 from .rendering import sprite
 
+def flatten_surface_color(surface:pygame.Surface, color:tuple):
+    flattened_surface = pygame.Surface(surface.get_size(), pygame.SRCALPHA)
+    mask = pygame.mask.from_surface(surface)
+    mask.to_surface(flattened_surface, setcolor=color)
+    return flattened_surface
+
 def flatten_sprite_colors(sprite:sprite, color:tuple):
     surfaces = []
     for surface in sprite.surfaces:
-        mask = pygame.mask.from_surface(surface)
-        new_surface = pygame.Surface(surface.get_size(), pygame.SRCALPHA)
-        mask.to_surface(new_surface, setcolor=color)
-
-        surfaces.append(new_surface)
+        surfaces.append(flatten_surface_color(surface, color))
     sprite.surfaces = surfaces
+
+def swap_colors_in_surface(surface:pygame.Surface, old_color:tuple, new_color:tuple):
+    old_surface:pygame.Surface = surface.copy()
+    old_surface.set_colorkey(old_color)
+    new_surface = pygame.Surface(old_surface.get_size())
+    new_surface.fill(new_color)
+    new_surface.blit(old_surface, (0, 0))
+    return new_surface
 
 def swap_colors_in_sprite(sprite:sprite, old_color:tuple, new_color:tuple):
     surfaces = []
     for surface in sprite.surfaces:
-        old_surface:pygame.Surface = surface.copy()
-        old_surface.set_colorkey(old_color)
-        new_surface = pygame.Surface(old_surface.get_size())
-        new_surface.fill(new_color)
-        new_surface.blit(old_surface, (0, 0))
-
-        surfaces.append(new_surface)
+        surfaces.append(swap_colors_in_surface(surface, old_color, new_color))
     sprite.surfaces = surfaces
 
 def draw_gradient_circle(color:tuple, radius:int):
@@ -35,3 +39,15 @@ def draw_gradient_circle(color:tuple, radius:int):
         pygame.draw.circle(surface, temp_color, (radius, radius), (radius - i))
 
     return surface
+
+def draw_outline_on_surface(surface:pygame.Surface, color:tuple, width:int):
+    outlined_surface = surface.copy()
+    surface_mask = pygame.mask.from_surface(surface)
+    pygame.draw.lines(outlined_surface, color, False, surface_mask.outline(), width)
+    return outlined_surface
+
+def draw_outline_on_sprite(sprite:sprite, color:tuple, width:int):
+    surfaces = []
+    for surface in sprite.surfaces:
+        surfaces.append(draw_outline_on_surface(surface, color, width))
+    sprite.surfaces = surfaces
